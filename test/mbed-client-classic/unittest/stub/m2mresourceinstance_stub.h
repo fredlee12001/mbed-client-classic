6647a73c159497685a919098c0f6adbdb33dc78d
Antti Yli-Tokola <antti.yli-tokola@arm.com>
2017-02-12 20:10:14 +0200

    Fix unit test compilation

--- test/mbed-client-classic/unittest/stub/m2mresourceinstance_stub.h
+++ test/mbed-client-classic/unittest/stub/m2mresourceinstance_stub.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 ARM Limited. All rights reserved.
+ * Copyright (c) 2015 ARM Limited. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the License); you may
  * not use this file except in compliance with the License.
@@ -32,3 +32,4 @@ namespace m2mresourceinstance_stub
 }
 
 #endif // M2M_RESOURCE_INSTANCE_STUB_H
+
